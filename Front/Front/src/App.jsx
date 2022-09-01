8c629c87557a5131799548c0949bd6dee380a5cc
Maru <marianalopezeikilis>
2022-09-01 13:37:23 +0200

    Avanzando con el front de home

--- Front/Front/src/App.jsx
+++ Front/Front/src/App.jsx
@@ -1,10 +1,12 @@
-import { useState } from 'react'
 import './App.css'
+import Home from './components/Home.jsx'
 
 function App() {
 
   return (
     <div className="App">
+    <Home/> 
+
     </div>
   )
 }
