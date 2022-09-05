47a7fba450b9cde90372e9dcc4fa4bd16cc03acd
Maru <marianalopezeikilis>
2022-09-05 09:49:54 +0200

    Cambiando el fondo

--- Front/Front/src/components/Home.jsx
+++ Front/Front/src/components/Home.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
 import '../App.css'
-import Button from 'react-bootstrap/Button';
+ import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 function Home() {
   return (
-<div>
-<h1> Nombre de la App</h1>
+<div class='divhome vh-100'>
+<h1> Fuentes y Cuidado Ambiental </h1>
 <img class='imgagua' src='/img/agua.jpg'/>
 <br></br>
+
+<br/>
 <Button class='btnfuente' variant='success'> Fuentes</Button>
 <Button class='btneconews'>EcoNews </Button>
     </div>
