e4c0afb62286fdaa518b014dc1638c9a651b623d
Maru <marianalopezeikilis>
2022-09-06 11:32:35 +0200

    Arreglado css responsive del home

--- Front/Front/src/components/Home.jsx
+++ Front/Front/src/components/Home.jsx
@@ -1,30 +1,43 @@
-import React from 'react'
-import '../App.css'
- import Button from 'react-bootstrap/Button';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import ButtonBlue from './ButtonBlue';
-import Container from 'react-bootstrap/Container';
-
-
+import React from "react";
+import "../App.css";
+import Button from "react-bootstrap/Button";
+import "bootstrap/dist/css/bootstrap.min.css";
+import ButtonBlue from "./ButtonBlue";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 function Home() {
   return (
-<Container fluid className='conthome'>
-<div className='contconthome'>
-<h1 className="h1home"> Water care </h1>
-<img className='imgagua' src='/img/logo02.png'/>
-<br/>
-<p className='texthome'> Descubre las novedades medioambientales en nuestro blog y encuentra las fuentes cercanas a ti para disfrutar del agua potable de acceso público y contribuir al mismo tiempo a nuestro planeta.</p>
+    <Container fluid className="containerhome">
+      <Row className="justify-content-center">
+        <Col xs="2" md="2" lg="4" className="colhome">
+          <h1 className="h1home"> Water care </h1>
+          <img className="imgagua" src="/img/logo02.png" />
+          <br />
+          <p className="texthome">
+            {" "}
+            Descubre las novedades medioambientales en nuestro blog y encuentra
+            las fuentes cercanas a ti para disfrutar del agua potable de acceso
+            público y contribuir al mismo tiempo a nuestro planeta.
+          </p>
 
-{/* <Button className='btnfuente'> Fuentes</Button>
-<Button className='btneconews'>EcoNews </Button> */}
-<ButtonBlue text='Blog' ruta='blog' />
-<br/>
-<ButtonBlue text='Fuentes' ruta='fuentes'/>
-    </div>
-  
+          <ButtonBlue text="Blog" ruta="blog" />
+          <br className="d-xl-none"/>
+          <ButtonBlue text="Fuentes" ruta="fuentes" />
+          
+        </Col>
+      </Row>
+{/* <Row> <Col>       
+          <div className="d-none d-xl-block divweb"> 
+Este div es para pantalla grande  
+</div></Col></Row> */}
+
+
     </Container>
-  )
+   
+
+  );
 }
 
-export default Home         
+export default Home;
