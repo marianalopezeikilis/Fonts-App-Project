78dac36eead659650a9ce7e1511306efffec86df
Maru <marianalopezeikilis>
2022-09-12 09:28:54 +0200

    corregido error

--- Front/Front/src/components/navbar.jsx
+++ Front/Front/src/components/navbar.jsx
@@ -1,11 +1,8 @@
 import { Container, Nav, Navbar } from 'react-bootstrap';
 import { BsArrowLeftShort, BsPlusLg } from "react-icons/bs";
-import {Link, useNavigate} from 'react-router-dom';
 
 import {Link, useNavigate} from 'react-router-dom';
 
-import { Link } from 'react-router-dom';
-import { useNavigate } from "react-router-dom";
 
 
 function NavBar() {
