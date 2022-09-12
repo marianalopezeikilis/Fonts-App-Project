4e937d1ee13f8c823145326af97fa44e60ca829f
Maru <marianalopezeikilis>
2022-09-12 09:27:49 +0200

    quitado los errores

--- Front/Front/src/components/navbar.jsx
+++ Front/Front/src/components/navbar.jsx
@@ -2,13 +2,12 @@ import { Container, Nav, Navbar } from 'react-bootstrap';
 import { BsArrowLeftShort, BsPlusLg } from "react-icons/bs";
 import {Link, useNavigate} from 'react-router-dom';
 
-<<<<<<< HEAD
 import {Link, useNavigate} from 'react-router-dom';
-=======
+
 import { Link } from 'react-router-dom';
 import { useNavigate } from "react-router-dom";
 
->>>>>>> 3bfef56ba8ad46eb56967e48de3950ead7c49d7c
+
 function NavBar() {
   const btn_style = { color: "E8F0F2", fontSize: "35px" }
   const btn_style02 = { color: "93BFD7", fontSize: "19px", marginTop: "-5px", marginRight: "5px" }
