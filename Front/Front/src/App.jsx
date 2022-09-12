d50d522723e714b2bf6bfc24390e5d700a5e18d4
Andres17X <andresaragon2001@gmail.com>
2022-09-12 10:52:16 +0200

    checkout

--- Front/Front/src/App.jsx
+++ Front/Front/src/App.jsx
@@ -2,6 +2,7 @@ import Home from './components/Home.jsx'
 import Map from "./components/map";
 import './App.css'
 
+
 function App() {
 
   return (
--- Front/Front/src/components/map.jsx
+++ Front/Front/src/components/map.jsx
@@ -1,79 +1,42 @@
-import { MapContainer, TileLayer, useMap, Marker, Popup, useMapEvents, GeoJSON, Circle } from 'react-leaflet'
+import { MapContainer, TileLayer, useMap, Marker, Popup, useMapEvents } from 'react-leaflet'
 import { render } from 'react-dom';
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import './map.css';
-import MarkerClusterGroup from '@changey/react-leaflet-markercluster';
-import 'react-leaflet-markercluster/dist/styles.min.css';
 import 'leaflet/dist/leaflet.css';
-import Ubicacion from './images/miUbi.png';
-import Fuentes from './Fuentes.json';
-import FuenteIco from './images/logo02.png';
+
+
 
 function Map() {
-  let loveIcon = new L.icon({
-    iconUrl: Ubicacion,
-    iconRetinaUrl: Ubicacion,
-    iconSize: [20, 20],
-    popupAnchor: [0, -10],
-  });
-
-  function LocationMarker() {
-    const [position, setPosition] = useState(null);
-
-    const map = useMap();
-
-    useEffect(() => {
-      map.locate().on("locationfound", function (e) {
-        setPosition(e.latlng);
-        map.flyTo(e.latlng, map.getZoom());
-      });
-    }, []);
-
-    return position === null ? null : (
-     
-      <Marker position={position} icon={loveIcon}>
-        <Popup>Estás aquí!</Popup>
-      </Marker>
-  
-    )
-
-    ;
-  }
-      let FuentesIcon =  L.icon({
-        iconUrl: FuenteIco,
-        iconRetinaUrl: FuenteIco,
-        iconSize: [40, 40],
-        popupAnchor: [0, -15],
-      });
+    function LocationMarker() {
+        const [position, setPosition] = useState(null)
+        const map = useMapEvents({
+          click() {
+            map.locate()
+          },
+          locationfound(e) {
+            setPosition(e.latlng)
+            map.flyTo(e.latlng, map.getZoom())
+          },
+        })
+      
+        return position === null ? null : (
+          <Marker position={position}>
+            <Popup>You are here</Popup>
+          </Marker>
+        )
+      }
+      
 
     return (
         <>
-
-
         <MapContainer
-          center={{ lat: 40.4167047, lng: -3.7035825 }}
-          zoom={15}
+          center={{ lat: 51.505, lng: -0.09 }}
+          zoom={13}
           scrollWheelZoom={false}>
           <TileLayer
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
-          <MarkerClusterGroup markers={FuentesIcon}>
-          <GeoJSON data={Fuentes}  pointToLayer={(feature, latlng) => {
-            if (Marker) {
-              return L.marker(latlng, { icon: FuentesIcon });
-            }
-            return L.marker(latlng);
-          }} 
-          onEachFeature= {(feature = {}, layer) => {
-            const { properties = {} } = feature;
-            const { name } = properties;
-    
-            if ( !name ) return;
-    
-            layer.bindPopup(`<p>${name}</p>`);}}
-            ></GeoJSON>
-          </MarkerClusterGroup>
           <LocationMarker />
         </MapContainer>
         </>
