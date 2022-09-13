3d82eb5a4dfa2e9c370302905a3901832dbb705f
Andres17X <andresaragon2001@gmail.com>
2022-09-13 11:34:58 +0200

    v2

--- Front/Front/src/components/map.jsx
+++ Front/Front/src/components/map.jsx
@@ -6,7 +6,7 @@ import MarkerClusterGroup from '@changey/react-leaflet-markercluster';
 import 'react-leaflet-markercluster/dist/styles.min.css';
 import 'leaflet/dist/leaflet.css';
 import Ubicacion from './images/miUbi.png';
-import Fuentes from './FuentesCat.json';
+import Fuentes from './Fuentes1.json';
 import FuenteIco from './images/logo02.png';
 
 function Map() {
