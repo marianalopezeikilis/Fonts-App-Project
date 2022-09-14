86ddcac58a249ba1b3e874a79f751029e9810f24
Andres17X <andresaragon2001@gmail.com>
2022-09-14 09:37:30 +0200

    map.jsx

--- Front/Front/src/main.jsx
+++ Front/Front/src/main.jsx
@@ -7,14 +7,16 @@ import App from './App'
 import Blog from "./Blog";
 import BlogLogin from "./BlogLogin";
 import BlogAdd from "./BlogAdd";
+import Map from './Map';
 
-//hola
+
 
 ReactDOM.render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />}/>
       <Route path="home" element={<App />}/>
+      <Route path="fuentes" element={<Map />}/>
       <Route path="blog" element={<Blog />} />
       <Route path="login" element={<BlogLogin />} />
       <Route path="add" element={<BlogAdd />} />
