e223b01551a9e25471b016ba5650b2803ebcfb57
Maru <marianalopezeikilis>
2022-09-14 10:52:45 +0200

    borrado loading de home

--- Front/Front/src/App.jsx
+++ Front/Front/src/App.jsx
@@ -1,22 +1,15 @@
 import Home from './components/Home.jsx'
-<<<<<<< HEAD
 import Map from "./components/map";
-import '././App.css'
-import Loading from './components/Loading.jsx';
-=======
+
 import './App.css'
->>>>>>> main
 
 
 function App() {
 
   return (
     <div className="App ">
-<<<<<<< HEAD
-<Loading/>
-=======
+
     <Home/>
->>>>>>> main
 
     </div>
   )
