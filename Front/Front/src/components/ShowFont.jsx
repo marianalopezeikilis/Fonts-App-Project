956e2c17214c8e2d39ff24a905fbac5b5f1fa5a6
Maru <marianalopezeikilis>
2022-09-16 09:23:52 +0200

    Modificando el ranking

--- Front/Front/src/components/ShowFont.jsx
+++ Front/Front/src/components/ShowFont.jsx
@@ -17,12 +17,24 @@ function ShowFont() {
     <Row>
    <Col>
 <form>
+{/* <Col xs="2" md="2" lg="4" className="colhome"> */}
+
 <div class="form-row">
-<div class="form-group col-md-6">
+<div class="form-group col-xs-2 col-md-2 col-lg-4">
     <label for="inputNombre">Nombre de la fuente</label>
     <input type="text" class="form-control" id="inputNombre" placeholder="Personaliza el nombre de la fuente"/>
   </div>
 </div>
+
+<div class="form-row">
+<div class="form-group col-xs-2 col-md-2 col-lg-4">
+    <label for="inputNombre">Nombre de la fuente</label>
+    <input type="image" class="form-control" id="inputNombre" placeholder="Personaliza el nombre de la fuente"/>
+  </div>
+</div>
+  <ImageUpload useFoto={[img, setImg]} />
+                <Button type="submit" variant="celeste">{"Add"}</Button>
+
 </form>
    </Col>
     </Row>
