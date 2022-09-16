65075e6b59ee1cf1128b0a6452213b3ea962fbde
Maru <marianalopezeikilis>
2022-09-16 10:57:33 +0200

    Fin

--- Front/Front/src/components/map.jsx
+++ Front/Front/src/components/map.jsx
@@ -1,66 +1,6 @@
-// import { MapContainer, TileLayer, useMap, Marker, Popup, useMapEvents } from 'react-leaflet'
-// import { render } from 'react-dom';
-// import {Link, useNavigate} from 'react-router-dom';
-// import { useState } from "react";
-// import { BsPlusLg } from "react-icons/bs";
-// import './map.css';
-// import 'leaflet/dist/leaflet.css';
-// import NavBar from "./navbar.jsx";
-// import '../App.css'
 
-
-
-// function Map() {
-
-//   const btn_style04 = { color: "E8F0F2", fontSize: "35px", marginTop: "13px" }
-
-//   function LocationMarker() {
-//     const [position, setPosition] = useState(null)
-//     const map = useMapEvents({
-//       click() {
-//         map.locate()
-//       },
-//       locationfound(e) {
-//         setPosition(e.latlng)
-//         map.flyTo(e.latlng, map.getZoom())
-//       },
-//     })
-
-//     return position === null ? null : (
-//       <Marker position={position}>
-//         <Popup>You are here</Popup>
-//       </Marker>
-//     )
-//   }
-
-
-//   return (
-//     <>
-//       <NavBar />
-//       <div class="position_map">
-//         <MapContainer
-//         center={{ lat: 51.505, lng: -0.09 }}
-//         zoom={13}
-//         scrollWheelZoom={false}>
-//         <TileLayer
-//           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-//           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-//         />
-//         <LocationMarker />
-//       </MapContainer>
-//       </div>
-//       <Link to="/new" replace>
-//         <div class="add_newfont">
-//           <BsPlusLg style={btn_style04}/>
-//         </div>
-//       </Link>
-//     </>
-
-
-//   )
-
-// } export default Map;
-
+import '../App.css'
+import { BsPlusLg } from "react-icons/bs";
 import { MapContainer, TileLayer, useMap, Marker, Popup, useMapEvents, GeoJSON, Circle } from 'react-leaflet'
 import { render } from 'react-dom';
 import { useState, useEffect } from "react";
@@ -71,8 +11,11 @@ import 'leaflet/dist/leaflet.css';
 import Ubicacion from './images/miUbi.png';
 import Fuentes from './FuentesCat(zip).json';
 import FuenteIco from './images/logo02.png';
+import NavBar from "./navbar.jsx";
+import {Link, useNavigate} from 'react-router-dom';
 
 function Map() {
+  const btn_style04 = { color: "E8F0F2", fontSize: "35px", marginTop: "13px" }
   let loveIcon = new L.icon({
     iconUrl: Ubicacion,
     iconRetinaUrl: Ubicacion,
@@ -111,7 +54,8 @@ function Map() {
 
     return (
         <>
-
+       <NavBar />
+      <div className="position_map">
 
         <MapContainer
           center={{ lat: 51.50853, lng: -0.12574 }}
@@ -139,6 +83,12 @@ function Map() {
           </MarkerClusterGroup>
           <LocationMarker />
         </MapContainer>
+        </div>  
+        <Link to="/new" replace>
+//         <div class="add_newfont">
+//           <BsPlusLg style={btn_style04}/>
+//         </div>
+//       </Link>
         </>
 
 
